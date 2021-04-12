1bc49ad4fe0f492356159db5fbdc5d41fef231fb
Da Wei <dawei1@illinois.edu>
2021-04-11 18:49:40 -0700

    Added more comments.

--- Dataloader_utils.py
+++ Dataloader_utils.py
@@ -7,6 +7,7 @@ def parse_dataset_csv(csv_path):
         col_name_list = []
         col_name_found = False
         for row in dataset_info:
+            # The first row is the col names.
             if (not col_name_found):
                 col_name_found = True
                 col_name_list = row
@@ -35,16 +36,19 @@ def select_subset(data_set, subset_size, seed=None):
     population = range(0, dataset_size - 1)
     chosen_idx = set()
     no_finding_class = 0
+    # Keep adding cases until we have enough
     while (True):
         remaining_size = subset_size - len(chosen_idx)
         if remaining_size <= 0:
+            # We have enough cases
             break
         idx_list = random.choices(population, k=remaining_size)
         for idx in idx_list:
+            # Let's focus on the frontal PA scan for now.
             if data_set[idx]['direction'] == 'Frontal' and data_set[idx]['type'] == 'PA':
                 if data_set[idx]['label'][0] > 0.5:
                     if no_finding_class >= subset_size / 14:
-                        # We have too many no finding classes
+                        # We have too many 'no finding' classes
                         continue
                     no_finding_class = no_finding_class + 1
                 chosen_idx.add(idx)
