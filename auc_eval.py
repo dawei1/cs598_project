f2b1d7de2ea1d2e0daecdaadcdd8d704736a61a7
ouyadi <ouyadi@gmail.com>
2021-04-21 00:08:35 -0400

    merge code

--- auc_eval.py
+++ auc_eval.py
@@ -4,35 +4,21 @@ import numpy as np
 from numpy import mean
 import torch
 import utils
-<<<<<<< HEAD
 from full_model import *
 from torch.utils.data.sampler import SubsetRandomSampler
-=======
 import Dataloader
 
->>>>>>> 2a518b15ddc88c77036b74a1a2a69049d1325c04
 
 # Random Default from HW
 RANDOM_STATE = 545510477
 
-<<<<<<< HEAD
 #input: X = training data and Y = corresponding labels, model = trained model
 #output: accuracy, auc
 def auc_eval(model, dataloader, k=5):
     
     acc_list = []
     auc_list = []
-=======
-def eval_model(model, val_dataloader):
->>>>>>> 2a518b15ddc88c77036b74a1a2a69049d1325c04
-    
-    model.eval()
-    y_pred = torch.LongTensor()
-    y_true = torch.LongTensor()
-
-<<<<<<< HEAD
     for train_index, test_index in kf.split(X):
-#         print("TRAIN:", train_index, "TEST:", test_index)
         X_train, X_test = X[train_index], X[test_index] 
         Y_train, Y_test = Y[train_index], Y[test_index]
         tensor_x = torch.Tensor(X_train)
@@ -50,25 +36,10 @@ def eval_model(model, val_dataloader):
         auc_list.append(auc)
         
     acc_mean = mean(acc_list)
-    auc_mean = mean(auc_list)
-=======
-    for image, label in val_dataloader:
-        output = model.forward(image)
-        #Are steps below needed here in EVAL as well?         
-        output = 1 - output
-        output = (output * 0.02) + 0.98
-        output = torch.prod(output, 3)
-        output = torch.prod(output, 2)
-        prediction = 1 - output
-        #############################################
-    
-        y_pred = torch.cat((y_pred,  prediction.detach().to('cpu')), dim=0)
-        y_true = torch.cat((y_true, label.detach().to('cpu')), dim=0)   
->>>>>>> 2a518b15ddc88c77036b74a1a2a69049d1325c04
-        
+    auc_mean = mean(auc_list)        
     auc = roc_auc_score(y_true, y_pred)
     
-    return y_pred, y_true, auc
+    return auc
 
 
 #input: X = training data and Y = corresponding labels, model = trained model
